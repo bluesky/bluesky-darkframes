a4a1c44aac8c800f330a033592680b86d6bcfe57
Maksim Rakitin <mrakitin@bnl.gov>
2023-05-15 15:06:43 -0400

    TST: redefine the `RE` fixture to use it the old way (`call_returns_result=False`)

--- bluesky_darkframes/tests/conftest.py
+++ bluesky_darkframes/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
-from bluesky.tests.conftest import RE as RE_bluesky  # noqa
+from bluesky.tests.conftest import RE  # noqa F401
 
 
-@pytest.fixture(scope="function", params=[False])  # call_returns_result=False
-def RE(RE_bluesky):  # noqa F811
-    return RE_bluesky
+@pytest.fixture(name="RE", scope="function", params=[False])
+def _RE(RE):  # noqa F811
+    return RE
