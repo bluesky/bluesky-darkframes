c09e32c0e9e64c20deda2ad50221026d18298364
Maksim Rakitin <mrakitin@bnl.gov>
2023-05-15 15:49:57 -0400

    TST: add a comment about the fixture

--- bluesky_darkframes/tests/conftest.py
+++ bluesky_darkframes/tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 from bluesky.tests.conftest import RE  # noqa F401
 
 
-@pytest.fixture(name="RE", scope="function", params=[False])
+@pytest.fixture(name="RE", scope="function", params=[False])  # call_returns_result=False
 def _RE(RE):  # noqa F811
     return RE
