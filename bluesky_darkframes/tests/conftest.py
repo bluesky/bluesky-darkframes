570d66f6e4904042881c68f08043206853abc019
Maksim Rakitin <mrakitin@bnl.gov>
2023-05-15 14:40:47 -0400

    TST: return correct fixture

--- bluesky_darkframes/tests/conftest.py
+++ bluesky_darkframes/tests/conftest.py
@@ -4,4 +4,4 @@ from bluesky.tests.conftest import RE as RE_bluesky  # noqa
 
 @pytest.fixture(scope="function", params=[False])  # call_returns_result=False
 def RE(RE_bluesky):  # noqa F811
-    return RE
+    return RE_bluesky
