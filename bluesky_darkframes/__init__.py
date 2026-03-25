56ce4b49d987004dba2c66cf9095d20dcced496d
Jamie <seniosh@gmail.com>
2026-03-25 16:04:19 -0400

    Update __init__.py
    Adding collect_asset_docs to _SnapshotShell so that it gets properly picked up by the RunBundlers as a device that needs its assets collected

--- bluesky_darkframes/__init__.py
+++ bluesky_darkframes/__init__.py
@@ -149,6 +149,9 @@ class _SnapshotShell:
     def describe_configuration(self):
         return self.__snapshot.describe_configuration()
 
+    def collect_asset_docs(self):
+        return self.__snapshot.collect_asset_docs()
+    
     def read(self):
         return self.__snapshot.read()
 
